5e428f9a4b59a6b8bb64d4e24c6175ec2d305e22
Yanick Champoux <yanick@babyl.dyndns.org>
2019-11-13 12:13:21 -0500

    make hook dirs configurable

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,31 +1,31 @@
-import {execSync, spawn} from 'child_process';
-import fp from 'lodash/fp';
-import path from 'path';
-import report from 'yurnalist';
-import fs from 'fs';
-import {Vaudeville, hookTypes} from '../vaudeville';
-import { emojify } from 'node-emoji';
+import { execSync, spawn } from "child_process";
+import fp from "lodash/fp";
+import path from "path";
+import report from "yurnalist";
+import fs from "fs";
+import { Vaudeville, hookTypes } from "../vaudeville";
+import { emojify } from "node-emoji";
 
 export default async function list(vaudeville: Vaudeville) {
-  report.log('Vaudeville directories');
+  report.log("Vaudeville directories");
   report.list(
-    '',
-    vaudeville.vaudeville_dirs.map(dir => {
+    "",
+    (await vaudeville.vaudeville_dirs).map(dir => {
       try {
         fs.statSync(dir).isDirectory;
       } catch {
-        dir = dir + ' (not present)';
+        dir = dir + " (not present)";
       }
       return dir;
-    }),
+    })
   );
 
-  report.log('');
+  report.log("");
 
   const hooks = await vaudeville.hooks;
 
-  if( Object.values(hooks).every( h => h.length === 0 ) ) {
-      report.log( emojify( "not a single hook found :cry:" ) );
+  if (Object.values(hooks).every(h => h.length === 0)) {
+    report.log(emojify("not a single hook found :cry:"));
   }
 
   for (const t of hookTypes) {
@@ -35,10 +35,10 @@ export default async function list(vaudeville: Vaudeville) {
 
     for (const h of hooks[t]) {
       const abstract = await h.abstract;
-      hints[h.name] = `${h.prettyDir}${abstract ? ' - ' + abstract : ''}`;
+      hints[h.name] = `${h.prettyDir}${abstract ? " - " + abstract : ""}`;
     }
 
     report.log(t);
-    report.list('', hooks[t].map(fp.get('name')), hints);
+    report.list("", hooks[t].map(fp.get("name")), hints);
   }
 }
