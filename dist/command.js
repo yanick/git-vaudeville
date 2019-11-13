6b945530c4cd1d63dd757781f54ebeced2945de4
Yanick Champoux <yanick@babyl.dyndns.org>
2019-11-12 19:45:05 -0500

    build

--- dist/command.js
+++ dist/command.js
@@ -6,25 +6,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = __importDefault(require("commander"));
 const vaudeville_1 = require("./vaudeville");
 const withInfo = (program) => {
-    program.command('info', { isDefault: true })
-        .description('list hooks for current repository')
+    program
+        .command("info", { isDefault: true })
+        .description("list hooks for current repository")
         .action(() => {
-        require('./commands/info').default(new vaudeville_1.Vaudeville());
+        require("./commands/info").default(new vaudeville_1.Vaudeville());
     });
 };
 const withRun = (program) => {
-    program.command('run <phase>')
-        .option('-i --stdin <input>', "input to be stdin'ed to the hooks")
-        .description('run the hooks of the given phase')
+    program
+        .command("run <phase>")
+        .option("-i --stdin <input>", "input to be stdin'ed to the hooks")
+        .description("run the hooks of the given phase")
         .action((phase, opts) => {
-        require('./commands/run').default(new vaudeville_1.Vaudeville(), phase, opts);
+        require("./commands/run")
+            .default(new vaudeville_1.Vaudeville(), phase, opts)
+            .catch(() => null);
     });
 };
 const withInstall = (program) => {
-    program.command('install')
-        .description('install git-vaudeville shims locally')
+    program
+        .command("install")
+        .description("install git-vaudeville shims locally")
         .action((phase, opts) => {
-        require('./commands/install').default(new vaudeville_1.Vaudeville(), phase, opts);
+        require("./commands/install").default(new vaudeville_1.Vaudeville());
     });
 };
 withInfo(commander_1.default);
--- dist/hook.js
+++ dist/hook.js
@@ -9,6 +9,7 @@ const colors_1 = __importDefault(require("./colors"));
 const fs_1 = __importDefault(require("fs"));
 const readline_1 = __importDefault(require("readline"));
 const child_process_1 = require("child_process");
+const node_emoji_1 = require("node-emoji");
 const yurnalist_1 = __importDefault(require("yurnalist"));
 class Hook {
     constructor(path) {
@@ -24,7 +25,7 @@ class Hook {
         return path_1.default.dirname(path_1.default.dirname(this.path));
     }
     get prettyDir() {
-        return this.dir.replace(process.env["HOME"], '~');
+        return this.dir.replace(process.env["HOME"], "~");
     }
     get asReport() {
         return new Promise(async (resolve) => {
@@ -33,8 +34,7 @@ class Hook {
     }
     get abstract() {
         const stream = fs_1.default.createReadStream(this.path);
-        const rl = readline_1.default.createInterface({ input: stream,
-            crlfDelay: Infinity });
+        const rl = readline_1.default.createInterface({ input: stream, crlfDelay: Infinity });
         return new Promise(async (resolve, reject) => {
             for await (const line of rl) {
                 const m = line.match(/ABSTRACT:\s*(.*)/);
@@ -48,16 +48,20 @@ class Hook {
         });
     }
     async run(stdin) {
-        yurnalist_1.default.info(`running hook ${this.name}`);
+        yurnalist_1.default.info(`running hook '${this.name}'`);
         const x = child_process_1.spawn(this.path, [], {
-            stdio: ['pipe', 'inherit', 'inherit']
+            stdio: ["pipe", "inherit", "inherit"]
         });
         x.stdin.write(stdin);
         x.stdin.end();
-        await new Promise((resolve) => {
-            x.on('close', resolve);
+        const result = await new Promise((resolve, reject) => {
+            x.on("close", code => resolve(code));
         });
-        yurnalist_1.default.success('');
+        if (result !== 0) {
+            yurnalist_1.default.error(node_emoji_1.emojify("oh noes! Hook failed :face_vomiting:"));
+            throw new Error("hook failed");
+        }
+        yurnalist_1.default.success("");
     }
 }
 exports.default = Hook;
