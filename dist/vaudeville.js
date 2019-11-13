d5a75864521dc7e6f59f3b72c4e2e2ae3ce34708
Yanick Champoux <yanick@babyl.dyndns.org>
2019-11-13 12:13:42 -0500

    build

--- dist/vaudeville.js
+++ dist/vaudeville.js
@@ -4,25 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
-const fp_1 = __importDefault(require("lodash/fp"));
 const fs_extra_1 = __importDefault(require("fs-extra"));
+const promise_1 = __importDefault(require("simple-git/promise"));
+const fp_1 = __importDefault(require("lodash/fp"));
 const hook_1 = __importDefault(require("./hook"));
 exports.hookTypes = [
-    'applypatch-msg',
-    'post-commit',
-    'pre-auto-gc',
-    'pre-rebase',
-    'commit-msg',
-    'post-merge',
-    'pre-commit',
-    'pre-receive',
-    'post-applypatch',
-    'post-receive',
-    'prepare-commit-msg',
-    'update',
-    'post-checkout',
-    'pre-applypatch',
-    'pre-push',
+    "applypatch-msg",
+    "post-commit",
+    "pre-auto-gc",
+    "pre-rebase",
+    "commit-msg",
+    "post-merge",
+    "pre-commit",
+    "pre-receive",
+    "post-applypatch",
+    "post-receive",
+    "prepare-commit-msg",
+    "update",
+    "post-checkout",
+    "pre-applypatch",
+    "pre-push"
 ];
 const getHooks = async (dir) => {
     let files = fp_1.default.flatten(await Promise.all(exports.hookTypes
@@ -34,21 +35,33 @@ const getHooks = async (dir) => {
     return fp_1.default.compact(await Promise.all(files.map((f) => fs_extra_1.default
         .access(f, fs_extra_1.default.constants.X_OK)
         .then(() => f)
-        .catch(() => ''))).catch(() => []));
+        .catch(() => ""))).catch(() => []));
 };
 class Vaudeville {
     constructor() { }
     get vaudeville_dirs() {
-        return [
-            path_1.default.join(process.env['HOME'], 'git/vaudeville'),
-            './.git/hooks/vaudeville',
-        ];
+        return (async () => {
+            let rawDirs = await promise_1.default()
+                .raw(["config", "vaudeville.dirs"])
+                .catch(() => null);
+            if (!rawDirs)
+                rawDirs = "~/git/vaudeville,./.git/hooks/vaudeville";
+            return rawDirs
+                .split(",")
+                .map(d => d.replace(/^~/, process.env["HOME"] || "~").replace("\n", ""));
+        })();
     }
     get hooks() {
-        return Promise.all(this.vaudeville_dirs.map(getHooks)).catch(() => [])
-            .then(fp_1.default.flatten).then(h => h.map(i => new hook_1.default(i)))
-            .then(fp_1.default.sortBy('name'))
-            .then(fp_1.default.groupBy('type'));
+        return new Promise(async (resolve, reject) => {
+            this.vaudeville_dirs
+                .then(dirs => Promise.all(dirs.map(d => getHooks(d).catch(() => []))))
+                .then(fp_1.default.flatten)
+                .then(h => h.map(i => new hook_1.default(i)))
+                .then(fp_1.default.sortBy("name"))
+                .then(fp_1.default.groupBy("type"))
+                .then((x) => resolve(x))
+                .catch(() => resolve({}));
+        });
     }
 }
 exports.Vaudeville = Vaudeville;
